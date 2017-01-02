87d6b0fa9c0c0e6ef7f1d108a3296a66ed674e4a
cal13 <cal13199@gmail.com>
2017-01-02 17:46:27 -0500

    Added Circular Reveal
    Added Circular Reveal to Fab Found

--- app/src/main/java/com/example/cal13/found/found.java
+++ app/src/main/java/com/example/cal13/found/found.java
@@ -2,12 +2,85 @@ package com.example.cal13.found;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Transition;
+import android.transition.TransitionInflater;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
-public class found extends AppCompatActivity {
+public class found extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_found);
+        setupEnterAnimation();
     }
+    private void setupEnterAnimation()
+    {
+        Transition transition = TransitionInflater.from(this).inflateTransition(R.transition.changebounds_with_arcmotion);
+        transition.setDuration(300);
+        getWindow().setSharedElementEnterTransition(transition);
+        transition.addListener(new Transition.TransitionListener() {
+            @Override
+            public void onTransitionStart(Transition transition) {
+
+            }
+
+            @Override
+            public void onTransitionEnd(Transition transition) {
+                animateRevealShow(findViewById(R.id.activity_found));
+            }
+
+            @Override
+            public void onTransitionCancel(Transition transition) {
+
+            }
+
+            @Override
+            public void onTransitionPause(Transition transition) {
+
+            }
+
+            @Override
+            public void onTransitionResume(Transition transition) {
+
+            }
+        });
+    }
+
+    private void animateRevealShow(final View viewRoot)
+    {
+        int cx = (viewRoot.getLeft() + viewRoot.getRight()) / 2;
+        int cy = (viewRoot.getTop() + viewRoot.getBottom()) / 2;
+        AnimationUtilities.animateRevealShow(this, viewRoot, findViewById(R.id.found_fab).getWidth() / 2, R.color.colorAccent,
+                cx, cy, new OnRevealAnimationListener()
+                {
+                    @Override
+                    public void onRevealHide()
+                    {
+
+                    }
+
+                    @Override
+                    public void onRevealShow()
+                    {
+                        initViews();
+                    }
+                });
+    }
+
+    private void initViews()
+    {
+        Animation animation = AnimationUtils.loadAnimation(this, android.R.anim.fade_in);
+        animation.setDuration(300);
+        findViewById(R.id.activity_found_container).startAnimation(animation);
+        findViewById(R.id.activity_found_container).setVisibility(View.VISIBLE);
+        findViewById(R.id.found_fab).setVisibility(View.GONE);
+
+
+    }
+
 }
--- app/src/main/java/com/example/cal13/found/home.java
+++ app/src/main/java/com/example/cal13/found/home.java
@@ -3,6 +3,8 @@ package com.example.cal13.found;
 import android.app.ActivityOptions;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -27,7 +29,16 @@ public class home extends AppCompatActivity {
                 Intent i = new Intent(home.this, found.class);
                 View s = findViewById(R.id.homeBottomText);
                 String t = findViewById(R.id.homeBottomText).getTransitionName();
-                ActivityOptions transitionActivityOptions = ActivityOptions.makeSceneTransitionAnimation(home.this,s , t);
+
+                View f = findViewById(R.id.found_fab);
+                String tf= findViewById(R.id.found_fab).getTransitionName();
+
+
+                Pair<View, String> p1 = Pair.create(f, tf);
+                Pair<View, String> p2 = Pair.create(s, t);
+
+
+                ActivityOptionsCompat transitionActivityOptions = ActivityOptionsCompat.makeSceneTransitionAnimation(home.this,p1, p2);
                 startActivity(i, transitionActivityOptions.toBundle());
             }
         });
